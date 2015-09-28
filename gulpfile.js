6a5a78b7f9134e0abd1ddcbf774ea34fab7ede17
John Baur <johnrbaur@gmail.com>
2015-09-28 00:27:44 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    jade = require('gulp-jade'),
+    ts = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps');
+    
+var OUT = 'www-build';
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'www-build'
+    });
+});
+
+gulp.task('jade', function() {
+    gulp.src('www/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest(OUT));
+});
+
+gulp.task('copyLib', function() {
+    gulp.src('www/lib/**/*.js')
+        .pipe(gulp.dest(OUT + '/lib'));
+});
+
+gulp.task('ts', function() {
+    gulp.src(['www/app/**/*.ts', 'typings/**/*.d.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            noImplicitAny: true,
+            out: 'all.js'
+        })).js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(OUT));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('www/**/*.jade', ['jade']);
+    gulp.watch('www/app/**/*.ts', ['ts']);
+});
+
+gulp.task('build', ['jade', 'copyLib', 'ts', 'watch']);
